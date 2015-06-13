3e6a13ac65e19774285346942d9428668bb824e8
Jean-Christophe Jameux <Echologie@gmail.com>
2015-06-13 10:46:38 +0200

    Finalizing comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,10 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The purpose of this couple of functions is to implement "matrices"
+## that can potentially carry along themselvas their own inverses
+## and a "solve" function which takes this kind of "matrices" as argument
+## and return the inverses with the minimum of calculation.
 
-## makeCacheMatrix implements a "matrix" able to carry along itsef its own inverse
-## by creating a list of functions which can set and get the value of the matrix and its inverse.
+## makeCacheMatrix implements a "matrix" by creating a list of functions
+## which can set and get the value of the (usual) matrix and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inverse <- NULL
@@ -22,6 +24,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## cacheSolve return the inverse of the "matrix" created above,
 ## using the cached value if available, or calculates it if not.
+## In the latter case, cacheSolve caches the inverse in the "matrix".
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
